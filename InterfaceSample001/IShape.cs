cd9e30bea24eca9fe829f93efa1bff2a883442f6
Zkaiyin <zkaiyin8@gmail.com>
2024-05-07 16:41:51 +0800

    加入專案檔案。

---
+++ InterfaceSample001/IShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSample001
+{
+    public interface IShape
+    {
+        double GetArea();
+    }
+}
